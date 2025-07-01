0dfeb78908f216965683dea16e88b7c95dbec14f
talhasifat <talhasifat123@gmail.com>
2025-07-01 16:08:32 +0600

    Add files via upload

---
+++ Gym Membership System/DAL/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Interfaces;
+using GymManagement.DAL.Models;
+using GymManagement.DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccess
+    {
+        public static IRepo<Member, int, Member> MemberData()
+        {
+            return new MemberRepo();
+        }
+    }
+}
---
+++ Gym Membership System/DAL/GymMembershipSystem.cs
@@ -0,0 +1,19 @@
+﻿using GymManagement.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.DAL
+{
+    public class GymMembershipSystem : DbContext
+    {
+        public GymMembershipSystem() : base("GymMembershipSystem") { }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
